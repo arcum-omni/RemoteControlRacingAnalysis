0be889a85177f11bf04ef74aa573d393170b57e8
arcum-omni <travis.l.eiler@gmail.com>
2019-12-04 14:09:49 -0800

    Functionality Improved

--- scripts/main.js
+++ scripts/main.js
@@ -4,35 +4,39 @@ var myChart = new Chart(ctx, {
     data: {
         labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
         datasets: [{
-                label: 'Nov 15th 2wd Stock Buggy A-Main',
+                label: '15Nov; 2wd Stock Buggy, A-Main',
                 data: [20.304, 18.858, 19.324, 18.502, 18.671, 17.954, 18.301, 18.724, 18.151, 17.719, 17.923, 18.515, 18.25, 18.567, 19.183, 18.585, 18.554],
                 borderColor: 'rgba(255, 0, 0,0.5)',
                 backgroundColor: 'rgba(255, 0, 0,0.5)',
                 borderWidth: 1,
                 fill: false
-            }, {
-                label: 'Nov 17th 2wd Stock Buggy A-Main',
+            },
+            {
+                label: '17Nov; 2wd Stock Buggy, A-Main',
                 data: [20.237, 18.173, 18.502, 18.414, 18.745, 19.075, 18.529, 18.162, 18.607, 18.737, 18.821, 18.762, 18.562, 17.851, 19.198, 18.463, 18.349],
                 borderColor: 'rgba(0, 255, 0,0.5)',
                 backgroundColor: 'rgba(0, 255, 0,0.5)',
                 borderWidth: 1,
                 fill: false
-            }, {
-                label: 'Nov 22th 2wd Stock Buggy A-Main',
+            },
+            {
+                label: '22Nov; 2wd Stock Buggy, A-Main',
                 data: [20.692, 18.56, 18.085, 17.92, 19.036, 18.699, 18.465, 18.348, 18.418, 18.489, 18.538, 18.192, 19.061, 18.205, 18.483, 18.274, 18.783],
                 borderColor: 'rgba(0, 0, 255,0.5)',
                 backgroundColor: 'rgba(0, 0, 255,0.5)',
                 borderWidth: 1,
                 fill: false
-            }, {
-                label: 'Nov 24th 2wd Stock Buggy A-Main',
+            },
+            {
+                label: '24Nov; 2wd Stock Buggy, A-Main',
                 data: [18.654, 17.718, 17.589, 17.706, 18.33, 18.105, 18.236, 18.178, 18.675, 18.154, 17.691, 17.54, 18.227, 18.338, 18.234, 18.314, 18.117],
-                borderColor: 'rgba(0, 0, 0,0.5)',
-                backgroundColor: 'rgba(0, 0, 0,0.5)',
+                borderColor: 'rgba(0, 0, 0,0.75)',
+                backgroundColor: 'rgba(0, 0, 0,0.75)',
                 borderWidth: 1,
                 fill: false
-            }, {
-                label: 'Nov 29th 2wd Stock Buggy A-Main',
+            },
+            {
+                label: '29Nov; 2wd Stock Buggy, A-Main',
                 data: [22.903, 18.35, 17.756, 22.483, 20.645, 19.637, 18.453, 18.596, 17.063, 17.159, 18.324, 22.487, 18.673, 17.664, 17.623, 21.991],
                 borderColor: 'rgba(0, 0, 0,0.25)',
                 backgroundColor: 'rgba(0, 0, 0,0.25)',
@@ -41,7 +45,48 @@ var myChart = new Chart(ctx, {
             }]
     },
     options: {
+        title: {
+            display: true,
+            postion: 'top',
+            text: 'Race Lap Times'
+        },
+        legend: {
+            position: 'bottom'
+        },
         scales: {
+            yAxes: [{
+                    ticks: {
+                        min: 15.0,
+                        max: 23.0
+                    }
+                }]
+        },
+    }
+});
+var ctx2 = document.getElementById('myChart2');
+var myChart2 = new Chart(ctx2, {
+    type: 'line',
+    data: {
+        labels: ['15Nov', '17Nov', '22Nov', '24Nov', '29Nov'],
+        datasets: [{
+                label: 'Fastest Lap',
+                data: [17.719, 17.851, 17.920, 17.540, 17.063],
+                borderColor: 'rgba(0, 0, 0,0.1)',
+                backgroundColor: ['rgba(255, 0, 0,0.5)', 'rgba(0, 255, 0,0.5)', 'rgba(0, 0, 255,0.5)', 'rgba(0, 0, 0,0.75)', 'rgba(0, 0, 0,0.25)'],
+                borderWidth: 1,
+                fill: false,
+                showline: false
+            }]
+    },
+    options: {
+        title: {
+            display: true,
+            postion: 'top',
+            text: 'Race Fastest Lap Time'
+        },
+        legend: {
+            position: 'bottom',
+        }, scales: {
             yAxes: [{
                     ticks: {
                         min: 15.0,
@@ -51,8 +96,8 @@ var myChart = new Chart(ctx, {
         }
     }
 });
-var ctx2 = document.getElementById('myChart2');
-var myChart2 = new Chart(ctx2, {
+var ctx3 = document.getElementById('myChart3');
+var myChart3 = new Chart(ctx3, {
     type: 'line',
     data: {
         labels: ['15Nov', '17Nov', '22Nov', '24Nov', '29Nov'],
@@ -63,22 +108,33 @@ var myChart2 = new Chart(ctx2, {
                 backgroundColor: 'rgba(255, 0, 0,0.5)',
                 borderWidth: 1,
                 fill: false
+            }, {
+                label: 'Lap Time Standard Deviation',
+                data: [0.421, 0.330, 0.305, 0.314, 1.824],
+                borderColor: 'rgba(0, 0, 0,0.5)',
+                backgroundColor: 'rgba(0, 0, 0,0.5)',
+                borderWidth: 1,
+                fill: false
             }]
     },
     options: {
-        scales: {
+        title: {
+            display: true,
+            postion: 'top',
+            text: 'Race Consistency & Standard Deviation'
+        },
+        legend: {
+            position: 'bottom',
+        }, scales: {
             yAxes: [{
                     ticks: {
-                        min: 0.84,
-                        max: 1.0
+                        min: 0.0,
+                        max: 2.0
                     }
                 }]
         }
     }
 });
-/**
- * Displays current year (1999) in the copyright statement
- */
 function getYear() {
     var today = new Date();
     var year = today.getFullYear();
@@ -88,3 +144,7 @@ function getYear() {
 window.onload = function () {
     getYear();
 };
+var player = new Plyr('#player', {
+    
+});
+window.player = player;
