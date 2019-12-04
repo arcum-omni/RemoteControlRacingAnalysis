43c25cbe76c75ea07bb18cba451c63f3d02fcd20
arcum-omni <travis.l.eiler@gmail.com>
2019-12-03 20:17:53 -0800

    Chart Functional wiht Hardcodee Data

--- scripts/main.js
+++ scripts/main.js
@@ -2,54 +2,37 @@ var ctx = document.getElementById('myChart');
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
         datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
+                label: 'Nov 15th 2wd Stock Buggy A-Main',
+                data: [20.304, 18.858, 19.324, 18.502, 18.671, 17.954, 18.301, 18.724, 18.151, 17.719, 17.923, 18.515, 18.25, 18.567, 19.183, 18.585, 18.554],
+                borderColor: 'rgba(255, 0, 0,0.5)',
+                backgroundColor: 'rgba(255, 0, 0,0.5)',
+                borderWidth: 1,
+                fill: false
+            }, {
+                label: 'Nov 17th 2wd Stock Buggy A-Main',
+                data: [20.237, 18.173, 18.502, 18.414, 18.745, 19.075, 18.529, 18.162, 18.607, 18.737, 18.821, 18.762, 18.562, 17.851, 19.198, 18.463, 18.349],
+                borderColor: 'rgba(0, 255, 0,0.5)',
+                backgroundColor: 'rgba(0, 255, 0,0.5)',
+                borderWidth: 1,
+                fill: false
+            }, {
+                label: 'Nov 22th 2wd Stock Buggy A-Main',
+                data: [20.692, 18.56, 18.085, 17.92, 19.036, 18.699, 18.465, 18.348, 18.418, 18.489, 18.538, 18.192, 19.061, 18.205, 18.483, 18.274, 18.783],
+                borderColor: 'rgba(0, 0, 255,0.5)',
+                backgroundColor: 'rgba(0, 0, 255,0.5)',
+                borderWidth: 1,
+                fill: false
+            }, {
+                label: 'Nov 24th 2wd Stock Buggy A-Main',
+                data: [18.654, 17.718, 17.589, 17.706, 18.33, 18.105, 18.236, 18.178, 18.675, 18.154, 17.691, 17.54, 18.227, 18.338, 18.234, 18.314, 18.117],
+                borderColor: 'rgba(0, 0, 0,0.5)',
+                backgroundColor: 'rgba(0, 0, 0,0.5)',
+                borderWidth: 1,
+                fill: false
             }]
     },
-    options: {
-        scales: {
-            yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-        }
-    }
-});
-var ctx = document.getElementById('myChart2').getContext('2d');
-var myChart2 = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'bar',
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-                label: 'My First dataset',
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: [0, 10, 5, 2, 20, 30, 45]
-            }]
-    },
-    // Configuration options go here
     options: {}
 });
 /**
